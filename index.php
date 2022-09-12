1f662aa0b1b8a428ddbea463cac31fc585e1f175
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-12 14:26:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 <?php
 $servername = "localhost";
 $username = "jeffreyn_homework3";
-$password = "MicahNight15";
+$password = "MicahNight2001";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -15,7 +44,7 @@ echo "Connected successfully";
 <?php
 $servername = "localhost";
 $username = "jeffreyn_homework3";
-$password = "MicahNight15";
+$password = "MicahNight2001";
 $dbname = "jeffreyn_homework3";
 
 // Create connection
@@ -31,6 +60,9 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
+    ?>
+<tr>
+  <td><?=$row["employee_id"]?></td>
     echo "id: " . $row["employee_id"]. " - Name: " . $row["employee_name"]."<br>";
   }
 } else {
@@ -38,3 +70,4 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+</table>
