d5033ae97e91af08578b94c8c53efe0a4a19a0b4
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-19 17:09:42 -0500

    Update managerfile.php

--- managerfile.php
+++ managerfile.php
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT employee_id, employee_name FROM Employee"
+$sql = "SELECT employee_id, employee_name FROM Employee";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
