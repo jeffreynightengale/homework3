f626fd90d45640fecaba6bc871e1fbaf5552cbeb
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-21 14:09:01 -0500

    Update managerfile.php

--- managerfile.php
+++ managerfile.php
@@ -2,7 +2,7 @@
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>$_POST["manager"]'s Employees</th>
+      <th><?php $_POST["manager"] ?>'s Employees</th>
     </tr>
   </thead>
   <tbody>
