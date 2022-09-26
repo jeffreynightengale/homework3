579327f1aca203ee1da5f7100de9bde52f064b0c
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-26 13:54:07 -0500

    Update supervisorfile.php

--- supervisorfile.php
+++ supervisorfile.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
 <?php require_once("header.php"); ?>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Supervisor's Managers</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
     <h1>Supervisor's Managers</h1>
 <table class="table table-striped">
   <thead>
