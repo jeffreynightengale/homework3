71dba179e434e82389da7b5cad5cfa668bfe1663
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-26 14:05:07 -0500

    Update managercust.php

--- managercust.php
+++ managercust.php
@@ -1,1 +1,49 @@
+<?php require_once("header.php"); ?>
+<h1>Manager Sells</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Customer Name</th>
+      <th>Product</th>
+      <th>Product Cost</th>
+      <th>Manager Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cid = $_POST['id'];
+//echo $iid;
+$sql = "select c.customer_name, c.product_name, c.product_cost, m.manager_name from customer c join employee e on c.customer_id = e.customer_id join manager m on e.manager_id = m.manager_id where m.manager_id=" . $cid;
+//echo $sql;
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["customer_id"]?></td>
+    <td><?=$row["product_name"]?></td>
+    <td><?=$row["product_cost"]?></td>
+    <td><?=$row["manager_name"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
