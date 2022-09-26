c57fe3f1c2eab2ce12444ed02d653aa3454b98d0
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-26 13:58:05 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
 <?php require_once("header.php"); ?>
-<h1>Hello, Everone!</h1>
+<h1>Hello, Everyone!</h1>
 <p>Welcome to the webpage to look at our employees, executives, and employee projects!</p>
 <?php require_once("footer.php"); ?>
